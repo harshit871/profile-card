d3d7a3da48756432f1d0322684521e8511153603
Harshit Soni <harshitashva@gmail.com>
2026-05-15 12:13:08 +0530

    constants

---
+++ src/assets/XIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const XIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    className="w-5 h-5"
+    viewBox="8 8 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M23.1707 9.875H25.9273L19.9048 16.7583L26.9898 26.125H21.4423L17.0973 20.4442L12.1257 26.125H9.36736L15.809 18.7625L9.01236 9.875H14.7007L18.6282 15.0675L23.1707 9.875ZM22.2032 24.475H23.7307L13.8707 11.4383H12.2315L22.2032 24.475Z"
+      fill="currentColor"
+    />
+  </svg>
+);
---
+++ src/constants.ts
@@ -0,0 +1,31 @@
+import { XIcon as X } from "./assets/XIcon";
+import { InstagramIcon as Instagram } from "./assets/InstagramIcon";
+import { GithubIcon as GitHub } from "./assets/GithubIcon";
+import { LinkedInIcon as LinkedIn } from "./assets/LinkedInIcon";
+
+export const socialLinks = [
+    {
+        name: "GitHub",
+        icon: GitHub,
+        ariaLabel: "Link to Sarah Dole's GitHub profile",
+        href: "https://github.com/sarahdole",
+    },
+    {
+        name: "LinkedIn",
+        icon: LinkedIn,
+        ariaLabel: "Link to Sarah Dole's LinkedIn profile",
+        href: "https://www.linkedin.com/in/sarahdole",
+    },
+     {
+        name: "Instagram",
+        icon: Instagram,
+        ariaLabel: "Link to Sarah Dole's Instagram profile",
+        href: "https://www.instagram.com/sarahdole",
+    },
+    {
+        name: "X",
+        icon: X,
+        ariaLabel: "Link to Sarah Dole's X profile",
+        href: "https://x.com/sarahdole"
+    },
+]
