5e2a932611ab849b79a12cbf5fe1abd88226fe9d
Harshit Soni <harshitashva@gmail.com>
2026-05-15 12:36:27 +0530

    fix: type

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,16 @@ import { XIcon as X } from "./assets/XIcon";
 import { InstagramIcon as Instagram } from "./assets/InstagramIcon";
 import { GithubIcon as GitHub } from "./assets/GithubIcon";
 import { LinkedInIcon as LinkedIn } from "./assets/LinkedInIcon";
+import type { JSX, SVGProps } from "react";
 
-export const socialLinks = [
+type SocialLink = {
+    name: string,
+    icon: (props: SVGProps<SVGSVGElement>) => JSX.Element,
+    ariaLabel: string,
+    href: string
+}
+
+export const socialLinks: SocialLink[] = [
     {
         name: "GitHub",
         icon: GitHub,
