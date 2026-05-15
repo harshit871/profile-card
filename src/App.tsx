ce9c998461f3306c3970ab41dc6f4556340cfb90
Harshit Soni <harshitashva@gmail.com>
2026-05-15 10:56:17 +0530

    feat: profile-card

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import ProfileCard from "./components/ProfileCard";
+import Credits from "./components/Credits";
+
+function App() {
+  return (
+    <>
+      <ProfileCard />
+      <Credits />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Credits.tsx
@@ -0,0 +1,23 @@
+const Credits = () => {
+  return (
+    <div className="credits" data-gfe-screenshot-exclude="true">
+      A challenge by &nbsp;
+      <a
+        href="https://www.greatfrontend.com/projects?ref=challenges"
+        target="_blank"
+      >
+        GreatFrontEnd Projects
+      </a>
+      . Built by &nbsp;
+      <a
+        href="https://www.greatfrontend.com/projects/u/harshitashva"
+        target="_blank"
+      >
+        Harshit Soni
+      </a>
+      .
+    </div>
+  );
+};
+
+export default Credits;
